8bbce16ee9686b1719dd1e3d0d6ebb32778b1b68
andrea <dev.mixicruz@gmail.com>
2023-07-06 09:35:26 -0600

    sube los componentes

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+export const home = () => {
+  const homeDiv = document.createElement('div');
+  const buttonRegis = document.createElement('button');
+
+  const buttonLogin = document.createElement('button');
+  buttonRegis.textContent = 'Registrate';
+  buttonLogin.textContent = 'Inicia sesión';
+
+  homeDiv.appendChild(buttonRegis);
+  homeDiv.appendChild(buttonLogin);
+
+  return homeDiv;
+}
