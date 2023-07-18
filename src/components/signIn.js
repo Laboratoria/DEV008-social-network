88eea68bd2ea5537ef5819158085d6760958b406
AndreaVazquez22 <kar.andre@icloud.com>
2023-07-18 13:10:32 -0600

    Importar funcion onNavigate y agregar botones

--- src/components/signIn.js
+++ src/components/signIn.js
@@ -1,10 +1,14 @@
-export const signIn = () => {
+export const signIn = (onNavigate) => {
   const homeDiv = document.createElement('div');
   const signUpButton = document.createElement('button');
   const signInButton = document.createElement('button');
 
   signUpButton.textContent = 'Registrate';
   signInButton.textContent = 'Inicia Sesión';
+
+  signUpButton.addEventListener('click', () => {
+    onNavigate('/signup');
+  });
 
   homeDiv.appendChild(signUpButton);
   homeDiv.appendChild(signInButton);
