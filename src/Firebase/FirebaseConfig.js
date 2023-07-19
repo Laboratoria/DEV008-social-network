ba4bb6d6445ecf8571ef5251c357ba40f2c06faf
andrea <dev.mixicruz@gmail.com>
2023-07-19 01:36:22 -0600

    se le dio estilo al Home y al register

---
+++ src/Firebase/FirebaseConfig.js
@@ -0,0 +1,35 @@
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-analytics.js";
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { valid } from "semver";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA3nkMmxJN-mrSa5VBxs62i1lpBv0wDOhA",
+  authDomain: "social-instapet.firebaseapp.com",
+  projectId: "social-instapet",
+  storageBucket: "social-instapet.appspot.com",
+  messagingSenderId: "1083258680295",
+  appId: "1:1083258680295:web:6ed3bd1f88ce0f51bb0342",
+  measurementId: "G-6H0S24JL2S"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig); 
+const analytics = getAnalytics(app);
+
+const auth = getAuth(firebaseApp);
+
+onAuthStateChanged(auth, user =>{
+    if user = null{
+        console.log('logged in!')
+    } else {
+        console.log('No user');
+    }
+});
