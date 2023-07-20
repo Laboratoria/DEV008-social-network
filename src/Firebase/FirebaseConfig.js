42b9bdee1e0f2b0b06cc91d464e2e72193c43fa6
andrea <dev.mixicruz@gmail.com>
2023-07-20 13:33:58 -0600

    guardar cambios de firebase y de login

--- src/Firebase/FirebaseConfig.js
+++ src/Firebase/FirebaseConfig.js
@@ -1,7 +1,8 @@
 /* eslint-disable import/no-unresolved */
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth';
+
+import { GoogleAuthProvider, getAuth, signInWithPopup } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
