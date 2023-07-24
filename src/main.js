7c95365aa9b99e6421eb7e8324249a640158d53d
AndreaVazquez22 <kar.andre@icloud.com>
2023-07-24 09:23:33 -0600

    Funciones Firebase y Comentarios en main

--- src/main.js
+++ src/main.js
@@ -1,29 +1,7 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
-import { getAnalytics } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-analytics.js';
 // rauter , gestiona las rutas
 import { signIn } from './components/signIn';
 import { signUp } from './components/signUp';
 import { wall } from './components/wall';
-// Import the functions you need from the SDKs you need
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyDhmNVMP2orY3iOmOT6DdaVABzLyzydVLY',
-  authDomain: 'redis-ccc00.firebaseapp.com',
-  projectId: 'redis-ccc00',
-  storageBucket: 'redis-ccc00.appspot.com',
-  messagingSenderId: '718676390172',
-  appId: '1:718676390172:web:516edc445106e832a98ad9',
-  measurementId: 'G-JQF50P4QXX',
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 const rootDiv = document.getElementById('root');
 // funciones a ejecutar por elección del usuario
@@ -32,25 +10,28 @@ const routes = {
   '/signup': signUp,
   '/wall': wall,
 };
-
+// onNavigate navegar entre paginas 
 const onNavigate = (pathname) => {
+// history es la historia de las pagina, pushState agrega una página nueva al historial y cambia el URL
   window.history.pushState({}, pathname, window.location.origin + pathname);
-  //while se utiliza si tengo que remover mas de un hijo
+  // while se utiliza si tengo que remover mas de un hijo
   // while (rootDiv.firstChild) {
   rootDiv.removeChild(rootDiv.firstChild);
+  // Limpiar la página 
   // }
-
+  // Agrega contenido del nuevo url (pathname)
   rootDiv.appendChild(routes[pathname](onNavigate));
 };
-//Selecciona pagina o componente para mostrarlo dependiendo URL
+// Selecciona pagina o componente para mostrarlo dependiendo URL
 const component = routes[window.location.pathname];
 console.log(window.location.pathname);
-//Recuperar las paginas cuando selecciono hacia atras o hacia adelante del historial(las saca del historial)
+
+// Recuperar las paginas cuando selecciono hacia atras o hacia adelante del historial(las saca del historial)
 window.addEventListener('popstate', () => {
   const paginas = routes[window.location.pathname];
   rootDiv.removeChild(rootDiv.firstChild);
   rootDiv.appendChild(paginas(onNavigate));
   console.log(window.location.pathname);
 });
-//Agrega el componente a la pantalla
+// Agrega el componente a la pantalla
 rootDiv.appendChild(component(onNavigate));
