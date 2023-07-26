1f2cbe9c7eee989c21861f2bac0ee6a81021331b
AndreaVazquez22 <kar.andre@icloud.com>
2023-07-26 15:32:19 -0600

    Login Google

--- src/components/firebase.js
+++ src/components/firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, addDoc} from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, addDoc } from 'firebase/firestore';
+import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 
 // Import the functions you need from the SDKs you need
 
@@ -16,14 +17,40 @@ const firebaseConfig = {
   messagingSenderId: '718676390172',
   appId: '1:718676390172:web:516edc445106e832a98ad9',
   measurementId: 'G-JQF50P4QXX',
-
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const provider = new GoogleAuthProvider();
+const auth = getAuth(app);
 
+export const googleLogin = () => {
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      const credential = GoogleAuthProvider.credentialFromResult(result);
+      const token = credential.accessToken;
+      // The signed-in user info.
+      const user = result.user;
+      // IdP data available using getAdditionalUserInfo(result)
+      // ...
+      console.log(user);
+    })
+    .catch((error) => {
+      // Handle Errors here.
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      // The email of the user's account used.
+      const email = error.customData.email;
+      // The AuthCredential type that was used.
+      const credential = GoogleAuthProvider.credentialFromError(error);
+      // ...
+      console.log(errorMessage);
+      console.log(errorCode);
+    });
+};
 export const savePosts = (post) => {
-addDoc(collection(db, 'posts'), {post})
+  addDoc(collection(db, 'posts'), { post });
 };
 export const getPosts = () => console.log('Get Posts');
