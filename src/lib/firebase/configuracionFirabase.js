53eb331fa7bfdc0c2f0387d8f93a7aee8f76e4d9
Kimberly Rv <elliis.rv@gmail.com>
2023-07-26 12:02:01 -0600

    acceder con google

--- src/lib/firebase/configuracionFirabase.js
+++ src/lib/firebase/configuracionFirabase.js
@@ -1,6 +1,5 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,10 +22,15 @@ const auth = getAuth(app);
 
 console.log(app.name);
 
-export const crearUsuario = (email,password) => {
-return createUserWithEmailAndPassword(auth, email, password)
+export const crearUsuario = (email, password) => {
+  return createUserWithEmailAndPassword(auth, email, password)
 };
 
 export const iniciarSesion = (email, password) => {
   return signInWithEmailAndPassword(auth, email, password)
+};
+
+export const accesoGoogle = () => {
+  const provider = new GoogleAuthProvider();
+  return signInWithPopup(auth, provider);
 };
