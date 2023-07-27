0b13af02f7fbd5fad4059c56f110cef65ed4c947
Mitzy Mora <mitzamora30@gmail.com>
2023-07-27 13:09:05 -0500

    media query

--- src/components/login.js
+++ src/components/login.js
@@ -1,4 +1,4 @@
-import { iniciarSesion } from "../lib/firebase/configuracionFirabase";
+import { iniciarSesion, accesoGoogle } from "../lib/firebase/configuracionFirabase";
 
 export const login = (onNavigate) => {
   const homeDiv = document.createElement('div');
@@ -88,6 +88,27 @@ export const login = (onNavigate) => {
         console.log(errorMessage);
         alert(error.message);
       });
+  });
+  buttonGoogle.addEventListener('click', (e) => {
+    e.preventDefault();
+    accesoGoogle().then((result) => {
+      const user = result.user;
+      console.log(user);
+      
+      alert('Bienvenido');
+      onNavigate('/feed');
+    }).catch((error) => {
+    
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode);
+      console.log(errorMessage);
+      // The email of the user's account used.
+      const email = error.customData.email;
+      console.log(email);
+      // ...
+      alert(error.message);
+    });
   });
 
   return homeDiv;
