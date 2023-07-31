c1ce081c1d854cc87609999933b3e1b6ad913044
GenovevaCastro <impresionesveva@gmail.com>
2023-07-31 17:28:04 -0600

    configuracion firestore

--- src/components/feed.js
+++ src/components/feed.js
@@ -1,5 +1,15 @@
 export const feed = (onNavigate) => {
-    const homeDiv = document.createElement('div');
+  const homeDiv = document.createElement('div');
+  homeDiv.classList.add('feedDiv');
+
+  const headerFeed = document.createElement('header');
+  headerFeed.classList.add('headerFeed');
+  homeDiv.appendChild(headerFeed);
+
+  const headerLogo = document.createElement('img');
+  headerLogo.src = '../logo.png';
+  headerLogo.classList.add('headerLogoFeed');
+  headerFeed.appendChild(headerLogo);
 
   const buttonCerrarSesion = document.createElement('button');
   buttonCerrarSesion.classList.add('buttonCerrarSesion');
